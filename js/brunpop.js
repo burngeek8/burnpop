23960e42b777b0a73fd3d077eb3aecef85f3f54b
Jose Luis Rodriguez <burngeek8@gmail.com>
2018-02-20 12:21:37 -0500

    derecha a izquierda y fondo solucionado

--- js/brunpop.js
+++ js/brunpop.js
@@ -1,31 +1,36 @@
 $(document).ready(function(){
-			console.log('ready');
-			var flash = $('.burnimg img'), 
-				izquierda = 0,
-				arriba = 0,
-				src;
+	console.log('ready');
+	var flash = $('.burnimg img'), 
+		derecha = 0,
+		arriba = 0,
+		src;
 
-			$("body").append(`<div class="burnpops"></div>`);					
-			for (var i = flash.length - 1; i >= 0; i--) {
-				src = $('.burnimg img:nth-child('+(i+1)+')').attr('src');	
-				$(".burnpops").append(`<div class="burnup">
-											<div class="btn-close"><i class="burn-close"></i></div>
-											<img src='${src}'>
-										  </div>`);			
-				//$('.burnup:nth-child('+(i+1)+')').addClass('active');
-				console.log(src);
-			}
-			for (var i = flash.length - 1; i >= 0; i--) {
-				arriba = arriba +  40;
-	            izquierda = izquierda + 100;
-				css = {'left' : izquierda,
-				       'top' : arriba};
-				$('.burnup:nth-child('+(i+1)+')').css(css);
-			}
-			$('.burnimg').remove();
-			$('.btn-close').on('click', burnclose);
-		});
-		function burnclose(){
-			$(this).parent().remove();
-			console.log('Probando cierre');
-		}
+	$("body").append(`<div class="burnpops"></div>`);					
+	for (var i = flash.length - 1; i >= 0; i--) {
+		src = $('.burnimg img:nth-child('+(i+1)+')').attr('src');	
+		$(".burnpops").append(`<div class="burnup">
+									<div class="btn-close"><i class="burn-close"></i></div>
+									<img src='${src}'>
+								  </div>`);			
+		//$('.burnup:nth-child('+(i+1)+')').addClass('active');
+		console.log(src);
+	}
+	for (var i = flash.length - 1; i >= 0; i--) {
+		arriba = arriba +  40;
+        derecha = derecha + 100;
+		css = {'right' : derecha,
+		       'top' : arriba};
+		$('.burnup:nth-child('+(i+1)+')').css(css);
+	}
+	$('.burnimg').remove();
+	$('.btn-close').on('click', burnclose);
+});
+function burnclose(){
+	$(this).parent().remove();
+	var flash = $('.burnup');
+
+	console.log('Probando cierre - quedan ' + flash.length);
+	if(flash.length == 0){
+		$('.burnpops').remove();
+	}
+}
